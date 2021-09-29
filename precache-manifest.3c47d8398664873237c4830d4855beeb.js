931dbb9a613638c8dd6cca91a504ac042c8cad30
Sergey Petrushkevich <sergeypetrushkevich@gmail.com>
2021-09-29 00:40:52 -0400

    Updates

---
+++ precache-manifest.3c47d8398664873237c4830d4855beeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76d8729c7abbeaceb99b",
+    "url": "/static/css/main.c7915ae1.chunk.css"
+  },
+  {
+    "revision": "76d8729c7abbeaceb99b",
+    "url": "/static/js/main.76d8729c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1494b47d2ddded544dda",
+    "url": "/static/js/2.1494b47d.chunk.js"
+  },
+  {
+    "revision": "3741659306500193767db4c3dc3cf68e",
+    "url": "/index.html"
+  }
+];
